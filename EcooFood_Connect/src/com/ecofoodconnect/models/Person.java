2693fd25e69f68ace8e0bcab176e30451f521595
Tanmay Pillai <pillai.ta@northeastern.edu>
2024-12-05 23:15:29 -0500

    ADDED: AuthService

--- EcooFood_Connect/src/com/ecofoodconnect/models/Person.java
+++ EcooFood_Connect/src/com/ecofoodconnect/models/Person.java
@@ -43,9 +43,25 @@ public class Person {
     public String getRole() {
         return role;
     }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
 
